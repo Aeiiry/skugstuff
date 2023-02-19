b10e11db250ff95fb6381d397731d37204eabc28
Micah Bradbury <micahnbrad@gmail.com>
2023-02-19 16:14:14 +1100

    different font handling

--- skug_ui.py
+++ skug_ui.py
@@ -2,28 +2,18 @@
 
 # main function
 import logging
-
+import sys
 import os
 import tkinter as tk
+from typing import Literal
 import pyglet
 from fontTools import ttLib
 
 # initialise logging
 logging.basicConfig(level=logging.DEBUG)
 
-
-def main() -> None:
-    """Main function"""
-
-    window: tk.Tk = create_window()
-    canvas: tk.Canvas = create_canvas(window)
-    skug_font: str = create_font("data/fonts/setznick-nf", "SelznickRemixNF.ttf")
-    label: tk.Label = create_label(window, skug_font, "Skug test COMBINOS bombino mama-mia", 100)
-
-    label.place(x=100, y=100)
-
-    # run the main loop
-    window.mainloop()
+FONT_SPECIFIER_NAME_ID: Literal[4] = 4
+FONT_SPECIFIER_FAMILY_ID: Literal[1] = 1
 
 
 def create_window() -> tk.Tk:
@@ -55,22 +45,48 @@ def create_font(path: str, font_name: str) -> str:
 
     # import the font from the given relative path str
     font_file: str = os.path.join(path, font_name)
-    pyglet.font.add_file(font_file)
+    try:
+        pyglet.font.add_file(font_file)  # type: ignore
+    except OSError:
+        logging.error("Font file not found")
+        sys.exit(1)
 
     # find the font name
-    font_facing_name: str = get_font_name(font_file)[0]
+    font_facing_name: str = get_font_name(font=ttLib.TTFont(font_file))[0]
 
     # return the font name
     return font_facing_name
 
 
-def get_font_name(font_file) -> tuple[str, str]:
-    """Get the font name"""
+def get_font_name(font: ttLib.TTFont) -> tuple[str, str]:
+    """Get the short name from the font's names table"""
+    # Initialize variables to hold the name and family
+    name: str = ""
+    family: str = ""
+    # Iterate over the font's name records
+    for record in font["name"].names:  # type: ignore
+        # Decode the name string from UTF-16 or UTF-8
+        if b"\x00" in record.string:
+            decoded_string: str = record.string.decode("utf-16-be")
+        else:
+            decoded_string = record.string.decode("utf-8")
 
-    font: ttLib.TTFont = ttLib.TTFont(font_file)
-    font_family_name = font["name"].getDebugName(1)  # type: ignore
-    font_full_name = font["name"].getDebugName(4)  # type: ignore
-    return font_family_name, font_full_name
+        # If the name record is the short name and we haven't found
+        # the short name yet, set the name to the decoded string
+        if record.nameID == FONT_SPECIFIER_NAME_ID and not name:
+            name = decoded_string
+
+        # If the name record is the family and we haven't found
+        # the family yet, set the family to the decoded string
+        elif record.nameID == FONT_SPECIFIER_FAMILY_ID and not family:
+            family = decoded_string
+
+        # If we have found both names, stop iterating
+        if name and family:
+            break
+
+    # Return both names as a tuple
+    return name, family
 
 
 def create_label(window: tk.Tk, skug_font: str, text: str, font_size: int) -> tk.Label:
@@ -87,6 +103,22 @@ def create_label(window: tk.Tk, skug_font: str, text: str, font_size: int) -> tk
 
     # return the label
     return label
+
+
+def main() -> None:
+    """Main function"""
+
+    window: tk.Tk = create_window()
+    canvas: tk.Canvas = create_canvas(window)
+    skug_font: str = create_font("data/fonts/setznick-nf", "SelznickRemixNF.ttf")
+    label: tk.Label = create_label(
+        window, skug_font, "Skug test COMBINOS bombino mama-mia", 100
+    )
+
+    label.place(x=100, y=100)
+
+    # run the main loop
+    window.mainloop()
 
 
 if __name__ == "__main__":
